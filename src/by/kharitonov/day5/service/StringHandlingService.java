0189d57b2a16eac97b2872e452a51bfe6f9259c9
Alexey <senator220291@gmail.com>
2020-06-26 14:53:24 +0300

    doing task

--- src/by/kharitonov/day5/service/StringHandlingService.java
+++ src/by/kharitonov/day5/service/StringHandlingService.java
@@ -1,4 +1,16 @@
 package by.kharitonov.day5.service;
 
+import java.util.List;
+
 public class StringHandlingService {
+    /*index starts from 1*/
+    public void replaceCharInWord(String text, int index,
+                                  char charReplacement) {
+        StringBuilder stringBuilder = new StringBuilder(text);
+        String []words = splitToWords(text);
+    }
+
+    private String [] splitToWords(String text) {
+        return text.split("[\\s\\n,.]");
+    }
 }
