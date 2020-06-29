f30ff462db199a15fdb7761e81c63aec0b13fa32
Alexey <senator220291@gmail.com>
2020-06-29 14:20:54 +0300

    modified exception class

--- src/by/kharitonov/day5/exception/TextHandlingException.java
+++ src/by/kharitonov/day5/exception/TextHandlingException.java
@@ -7,4 +7,12 @@ public class TextHandlingException extends Exception {
     public TextHandlingException(String message) {
         super(message);
     }
+
+    public TextHandlingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TextHandlingException(Throwable cause) {
+        super(cause);
+    }
 }
