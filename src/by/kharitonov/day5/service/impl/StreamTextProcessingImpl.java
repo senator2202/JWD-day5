08703c7718bc44892e968df13b6a05312e26da91
senator2202 <Nuttertools220291>
2020-07-02 17:04:21 +0300

    doing streams

--- src/by/kharitonov/day5/service/impl/StreamTextProcessingImpl.java
+++ src/by/kharitonov/day5/service/impl/StreamTextProcessingImpl.java
@@ -9,33 +9,61 @@ public class StreamTextProcessingImpl implements TextProcessing {
     private static final String REGEX_SPLIT_WORDS =
             "\\s*(\\s|,|!|\\.|;|:|\\?)\\s*";
     private static final String WORD_BOUNDARY = "\\b";
+    private static final String REGEX_NOT_TARGET_BEFORE = "[^(";
+    private static final String REGEX_NOT_TARGET_AFTER = ")]";
+    private static final String REGEX_SPLIT_NOT_SPACE_LETTER;
+
+    static {
+        String ls = System.getProperty("line.separator");
+        REGEX_SPLIT_NOT_SPACE_LETTER = "[à-ÿÀ-ßa-zA-Z¸¨\\s&&[^" + ls + "]]+";
+    }
 
     public String replaceCharInWord(String text, int index,
                                     char charReplacement) {
-        String[] s = {text};
+        String[] result = {text};
         Arrays.stream(text.split(REGEX_SPLIT_WORDS))
                 .filter(word -> word.length() >= index).distinct()
                 .collect(Collectors.toMap(word -> WORD_BOUNDARY + word + WORD_BOUNDARY, word ->
                         word.substring(0, index - 1) + charReplacement + word.substring(index)))
-                .forEach((k, v) -> s[0] = s[0].replaceAll(k, v));
-        return s[0];
+                .forEach((k, v) -> result[0] = result[0].replaceAll(k, v));
+        return result[0];
     }
 
     @Override
     public String replaceOneWithAnother(String text, String target,
                                         String replacement) {
-        return null;
+        String[] result = {text};
+        Arrays.stream(text.split(REGEX_NOT_TARGET_BEFORE + target + REGEX_NOT_TARGET_AFTER))
+                .filter(word -> word.contains(target)).distinct()
+                .collect(Collectors.toMap(word -> word, word -> replacement))
+                .forEach((k, v) -> result[0] = result[0].replaceAll(k, v));
+        return result[0];
     }
 
     @Override
     public String replaceWordsToSubstring(String text, int wordLength,
                                           String substring) {
-        return null;
+        String[] result = {text};
+        Arrays.stream(text.split(REGEX_SPLIT_WORDS))
+                .filter(word -> word.length() == wordLength).distinct()
+                .collect(Collectors.toMap(word -> WORD_BOUNDARY + word + WORD_BOUNDARY,
+                        word -> substring))
+                .forEach((k, v) -> result[0] = result[0].replaceAll(k, v));
+        return result[0];
     }
 
     @Override
     public String deleteAllNotSpaceOrLetter(String text) {
-        return null;
+        String[] result = {text};
+        Arrays.stream(text.split(REGEX_SPLIT_NOT_SPACE_LETTER))
+                .filter(symbals -> !symbals.isEmpty()).distinct()
+                .collect(Collectors.toMap(symbals -> symbals, symbals -> " "))
+                .forEach((k, v) -> result[0] = result[0].replaceAll(ignoreMeta(k), v));
+        return result[0];
+    }
+
+    private String ignoreMeta(String data) {
+        return "\\Q" + data + "\\E";
     }
 
     @Override
--- test/test/kharitonov/day5/service/impl/StreamTextProcessingImplTest.java
+++ test/test/kharitonov/day5/service/impl/StreamTextProcessingImplTest.java
@@ -16,4 +16,29 @@ public class StreamTextProcessingImplTest {
         String actualTextText = processor.replaceCharInWord(sourceText, 2, '!');
         assertEquals(actualTextText, expectedText);
     }
+
+    @Test
+    public void testReplaceOneWithAnother() {
+        String sourceText = TextProcessingData.SOURCE_TEXT;
+        String expectedText = TextProcessingData.REPLACED_ONE_TO_ANOTHER;
+        String actualText = processor.replaceOneWithAnother(sourceText, "–¿", "–Œ");
+        assertEquals(actualText, expectedText);
+    }
+
+    @Test
+    public void testReplaceWordsToSubstring() {
+        String sourceText = TextProcessingData.SOURCE_TEXT;
+        String expectedText = TextProcessingData.REPLACED_WORD_TO_SUBSTRING;
+        String actualText = processor.
+                replaceWordsToSubstring(sourceText, 7, "«¿Ã≈Õ¿");
+        assertEquals(actualText, expectedText);
+    }
+
+    @Test
+    public void testDeleteAllNotSpaceOrLetter() {
+        String sourceText = TextProcessingData.SOURCE_TEXT;
+        String expectedText = TextProcessingData.DELETED_NOT_LETTERS;
+        String actualText = processor.deleteAllNotSpaceOrLetter(sourceText);
+        assertEquals(actualText, expectedText);
+    }
 }
