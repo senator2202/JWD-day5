fd54d1e1a60fcb89935aaf4935f67fb3def41670
Alexey <senator220291@gmail.com>
2020-07-02 14:44:54 +0300

    started stream branch

--- src/by/kharitonov/day5/service/impl/StreamTextProcessingImpl.java
+++ src/by/kharitonov/day5/service/impl/StreamTextProcessingImpl.java
@@ -8,14 +8,15 @@ import java.util.stream.Collectors;
 public class StreamTextProcessingImpl implements TextProcessing {
     private static final String REGEX_SPLIT_WORDS =
             "\\s*(\\s|,|!|\\.|;|:|\\?)\\s*";
+    private static final String WORD_BOUNDARY = "\\b";
 
     public String replaceCharInWord(String text, int index,
                                     char charReplacement) {
         String[] s = {text};
         Arrays.stream(text.split(REGEX_SPLIT_WORDS))
                 .filter(word -> word.length() >= index).distinct()
-                .collect(Collectors.toMap(word -> "\\b" + word + "\\b", word -> word.substring(0,
-                        index - 1) + charReplacement + word.substring(index)))
+                .collect(Collectors.toMap(word -> WORD_BOUNDARY + word + WORD_BOUNDARY, word ->
+                        word.substring(0, index - 1) + charReplacement + word.substring(index)))
                 .forEach((k, v) -> s[0] = s[0].replaceAll(k, v));
         return s[0];
     }
